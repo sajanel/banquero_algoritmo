d56d32a0dc13d0348e223404d1dd4d15bea2a380
PC <PC@DESKTOP-QORBUC8>
2021-10-12 08:40:57 +0200

    Agregar archivos de proyecto.

---
+++ banquero_algoritmo/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace banquero_algoritmo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.bntAceptar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.comboBox1.Location = new System.Drawing.Point(102, 73);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(59, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.comboBox2.Location = new System.Drawing.Point(198, 73);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(59, 23);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(323, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Algoritmo del banquero";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(147, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dimension";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(178, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "X";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(70, 170);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(321, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(443, 170);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.Size = new System.Drawing.Size(321, 150);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView3.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(70, 350);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 25;
+            this.dataGridView3.Size = new System.Drawing.Size(323, 72);
+            this.dataGridView3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(178, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Matriz asignados";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(557, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Matriz maxima";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(166, 326);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 21);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Matriz disponible";
+            // 
+            // bntAceptar
+            // 
+            this.bntAceptar.Enabled = false;
+            this.bntAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bntAceptar.Location = new System.Drawing.Point(370, 64);
+            this.bntAceptar.Name = "bntAceptar";
+            this.bntAceptar.Size = new System.Drawing.Size(75, 38);
+            this.bntAceptar.TabIndex = 11;
+            this.bntAceptar.Text = "Aceptar";
+            this.bntAceptar.UseVisualStyleBackColor = true;
+            this.bntAceptar.Click += new System.EventHandler(this.bntAceptar_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(289, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Crear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(443, 358);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(257, 64);
+            this.listBox1.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(495, 334);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(156, 21);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Recursos disponibles";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bntAceptar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button bntAceptar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ banquero_algoritmo/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace banquero_algoritmo
+{
+    public partial class Form1 : Form
+    {
+        private int[,] necesarios;
+        private int[,] asignados;
+        private int[,] maximos;
+        private int[,] disponible;
+        private int numeroProcesos, numeroRecursos;
+        String nombreValor;
+
+        private void bntAceptar_Click(object sender, EventArgs e)
+        {
+
+            necesarios = new int [numeroProcesos,numeroRecursos];
+            asignados = new int[numeroProcesos, numeroRecursos];
+            maximos = new int[numeroProcesos, numeroRecursos];
+            disponible = new int[1, numeroRecursos];
+            
+            // Informacion para asignados
+            for (int i = 0; i < numeroProcesos; i++)
+            {
+                for (int j = 0; j < numeroRecursos; j++)
+                {
+                    //Rows[fila].Cells[col].Value.ToString();
+                    nombreValor = dataGridView1.Rows[i].Cells[j].Value.ToString();           
+                    asignados[i, j] = int.Parse(nombreValor);
+                }
+            }
+
+            // Informacion para Maximos
+            for (int i = 0; i < numeroProcesos; i++)
+            {
+                for (int j = 0; j < numeroRecursos; j++)
+                {
+                    nombreValor = dataGridView2.Rows[i].Cells[j].Value.ToString();
+                    maximos[i, j] = int.Parse(nombreValor);
+                }
+            }
+
+            // Ultima tabla 
+            for (int j = 0; j < this.numeroRecursos; j++)
+            {
+                nombreValor = dataGridView3.Rows[0].Cells[j].Value.ToString();
+                disponible[0, j] = int.Parse(nombreValor);  //matriz de disponibles
+            }
+
+            esSeguro();
+            bntAceptar.Enabled = false;
+        }
+
+        private int[,] calculoNecesarios()
+        {
+            for (int i = 0; i < numeroProcesos; i++)
+            {
+                for (int j = 0; j < numeroRecursos; j++) //calculando matriz de necesarios
+                {
+                    necesarios[i,j] = maximos[i,j] - asignados[i,j];
+                }
+            }
+
+            return necesarios;
+        }
+
+        private Boolean chequear(int i)
+        {
+            //chequeando si todos los recursos para el proceso pueden ser asignados
+            for (int j = 0; j < numeroRecursos; j++)
+            {
+                if (disponible[0,j] < necesarios[i,j])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Crear las tablas 
+        private void button1_Click(object sender, EventArgs e)
+        {
+             numeroProcesos = comboBox1.SelectedIndex + 1;
+             numeroRecursos = comboBox2.SelectedIndex + 1;
+            limpiar();
+
+            dataGridView1.ColumnCount = numeroRecursos;
+            dataGridView1.RowCount = numeroProcesos;
+          
+            dataGridView2.ColumnCount = numeroRecursos;
+            dataGridView2.RowCount = numeroProcesos;
+
+            dataGridView3.ColumnCount = numeroRecursos;
+            dataGridView3.RowCount = 1;
+
+            bntAceptar.Enabled = true;
+            listBox1.Items.Clear();
+        }
+        public void limpiar() 
+        {
+            dataGridView1.ClearSelection();
+            dataGridView2.ClearSelection();
+            dataGridView3.ClearSelection();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+           
+      
+        }
+
+        public void esSeguro()
+        {
+           
+            calculoNecesarios();
+            Boolean[] done = new Boolean[numeroProcesos];
+            int j = 0;
+
+            while (j < numeroProcesos)
+            {  //hasta que todos los procesos se asignen
+                Boolean asignado = false;
+                for (int i = 0; i < numeroProcesos; i++)
+                {
+                    if (!done[i] && chequear(i))
+                    {  //intentando asignar
+                        for (int k = 0; k < numeroRecursos; k++)
+                        {
+                           disponible[0,k] = disponible[0,k] - necesarios[i,k] + maximos[i,k];
+                        }
+                        listBox1.Items.Add("Proceso asignado :" + i);
+                        asignado = done[i] = true;
+                        j++;
+                    }
+                }
+                if (!asignado)
+                {
+                    break;  //si no esta asignado
+                }
+            }
+            if (j == numeroProcesos) //si todos los procesos estan asignados
+            {
+                MessageBox.Show("Asignado de forma segura");
+            }
+            else
+            {
+                MessageBox.Show("Todos los procesos se pueden asignar de forma segura");
+            }
+
+        }
+        }
+}
